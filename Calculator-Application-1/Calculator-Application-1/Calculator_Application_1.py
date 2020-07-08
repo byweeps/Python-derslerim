94f68bb1056994f60f9749f34da88c42d38aa6e2
byweeps <60719383+byweeps@users.noreply.github.com>
2020-07-09 01:53:01 +0300

    Calculator-Application-1

---
+++ Calculator-Application-1/Calculator-Application-1/Calculator_Application_1.py
@@ -0,0 +1,10 @@
+#Hesap Makinesi
+
+a=int(input("Enter the 1st issue : "))
+b=int(input("Enter the 2st issue : "))
+
+print("Toplam  : ",a+b)
+print("Çıkarma : ",a-b)
+print("Çarpma  : ",a*b)
+print("Bölme   : ",float(a/b))
+
