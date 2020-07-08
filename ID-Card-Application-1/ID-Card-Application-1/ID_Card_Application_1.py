8fbb1f4526605208bd49172d77d00ef5c60ffc48
byweeps <60719383+byweeps@users.noreply.github.com>
2020-07-08 22:27:55 +0300

    ID-Card-Aplication-1

---
+++ ID-Card-Application-1/ID-Card-Application-1/ID_Card_Application_1.py
@@ -0,0 +1,13 @@
+name="Jack"
+surname="Adrian"
+ID_number="858-525-585"
+born="USA"
+state="OH"
+birtday="04/11/1989"
+
+print("Your Name       : ",name)
+print("Your Surname    : ",surname)
+print("Your ID Number  : ",ID_number)
+print("Your Place Born : ",born)
+print("Your State Be   : ",state)
+print("Your Birtday    : ",birtday)
